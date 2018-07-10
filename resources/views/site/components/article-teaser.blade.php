eb38b39550d2f98e13f061fcbb3d08c09c6f6f7b
VacaSan <kotaracvladan@gmail.com>
2018-07-10 13:06:37 +0200

    Update teaser styles.

--- resources/views/site/components/article-teaser.blade.php
+++ resources/views/site/components/article-teaser.blade.php
@@ -1,5 +1,6 @@
 @php
   $isFeatured = isset($featured) ? $featured : false;
+  $hasActions = isset($actions) ? $actions : false;
   $teaser = 'teaser'.($isFeatured ? ' teaser--featured' : '');
   $image = 'image js-lazy-image image--'.($isFeatured ? '21-9' : '1-1');
   $details = 'details'.($isFeatured ? ' details--fixed' : '');
@@ -21,12 +22,14 @@
   <div class="teaser_primary">
     <div class="{{$details}}">
       <a href="#" class="details-item details-item--primary">category</a>
-      <span class="details-item details-item--secondary">date</span>
+      <span class="details-item details-item--secondary">12/3/2018</span>
     </div>
     <h3 class="teaser_title"><a href="#">Article title</a></h3>
     <p class="{{$body}}">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
   </div>
+  @if($hasActions)
   <div class="teaser_actions">
     <a href="#" class="btn btn--secondary">more</a>
   </div>
+  @endif
 </div>
