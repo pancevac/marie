d546916a59293a4c42ea677a7b315469691f213a
VacaSan <kotaracvladan@gmail.com>
2018-07-10 08:41:16 +0200

    Image styles.

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -7,8 +7,10 @@
         <h1>main stuff</h1>
         <button class="btn">button</button>
         <a href="#" class="btn">link</a>
-        <input type="button" value="input" class="btn">
-        <button class="btn btn--primary">button</button>
+        
+        <div class="image image--16-9">
+
+        </div>
       </div>
       <aside class="content-aside">side stuff</aside>
     </div>
