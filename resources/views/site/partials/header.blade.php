3bad88186d64407b7d52072974189b77b52b86e1
VacaSan <kotaracvladan@gmail.com>
2018-07-11 12:42:33 +0200

    Logo basic styles.

--- resources/views/site/partials/header.blade.php
+++ resources/views/site/partials/header.blade.php
@@ -1,11 +1,20 @@
 <header class="header">
-  <div class="container header-wrap">
+  <div class="container header-wrap" style="display: flex;">
+    
     <button class="icon-btn">
       <svg class="icon" role="presentation">
         <use xlink:href="#icon_menu"></use>
       </svg>
     </button>
 
+    <div style="text-align: center;">
+      <a href="/" style="color: inherit;">
+        <svg class="logo logo--header">
+          <use xlink:href="#logo"></use>
+        </svg>
+      </a>
+    </div>
+  
     <tab-bar>
       <div class="nav-item">
         <a class="nav-link" href="#">moda</a>
