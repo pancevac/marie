8b56fc82e592626f3c4b10e8fca7c4beedf049db
VacaSan <kotaracvladan@gmail.com>
2018-07-11 11:09:08 +0200

    Extracted header.

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -9,29 +9,7 @@
 </head>
 <body>
   <div id="app" class="content">
-    <header class="header">
-      <div class="container header-wrap">
-
-        <tab-bar>
-          <div class="nav-item">
-            <a class="nav-link" href="#">moda</a>
-          </div>
-          <div class="nav-item">
-            <a class="nav-link" href="#">lepota</a>
-          </div>
-          <div class="nav-item">
-            <a class="nav-link" href="#">lifestyle</a>
-          </div>
-          <div class="nav-item">
-            <a class="nav-link" href="#">ljubav</a>
-          </div>
-          <div class="nav-item">
-            <a class="nav-link" href="#">biznis</a>
-          </div>
-        </tab-bar>
-
-      </div>
-    </header>
+    @include('site.partials.header')
 
     @yield('content')
   </div>
---
+++ resources/views/site/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header class="header">
+  <div class="container header-wrap">
+
+    <tab-bar>
+      <div class="nav-item">
+        <a class="nav-link" href="#">moda</a>
+      </div>
+      <div class="nav-item">
+        <a class="nav-link" href="#">lepota</a>
+      </div>
+      <div class="nav-item">
+        <a class="nav-link" href="#">lifestyle</a>
+      </div>
+      <div class="nav-item">
+        <a class="nav-link" href="#">ljubav</a>
+      </div>
+      <div class="nav-item">
+        <a class="nav-link" href="#">biznis</a>
+      </div>
+    </tab-bar>
+
+  </div>
+</header>
