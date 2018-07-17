4e8eb28bc1e3583247ccd4352000d9ae20055019
VacaSan <kotaracvladan@gmail.com>
2018-07-17 15:57:32 +0200

    Footer navigation.

--- resources/views/site/partials/footer.blade.php
+++ resources/views/site/partials/footer.blade.php
@@ -1,5 +1,17 @@
 <footer class="footer">
-  <div class="container">
-    footer
+  <div class="container footer_container">
+    <div>logo and stuff</div>
+    <div class="footer_nav">
+      <ul class="footer_nav-list">
+        <li><a href="#">newsletter</a></li>
+        <li><a href="#">moda</a></li>
+        <li><a href="#">lepota</a></li>
+        <li><a href="#">lifestyle</a></li>
+        <li><a href="#">ljubav</a></li>
+        <li><a href="#">biznis</a></li>
+        <li><a href="#">beauty box</a></li>
+      </ul>
+    </div>
+    <div>not sure..</div>
   </div>
 </footer>
