c380c77778c76e50249590911f9af98e4e332791
VacaSan <kotaracvladan@gmail.com>
2018-07-18 10:18:18 +0200

    Extracted pagination partial.

--- resources/views/site/pages/category.blade.php
+++ resources/views/site/pages/category.blade.php
@@ -25,6 +25,8 @@
           
         </div>
 
+        @include('site.partials.pagination')
+
       </div>
       <aside class="content-aside">
         <div class="aside-box" style="width:300px; height:300px; background:hotpink;">banner</div>
@@ -32,28 +34,6 @@
           @include('site.partials.newsletter')
         </div>
       </aside>
-    </div>
-
-    <div class="container">
-      <nav aria-label="page navigation">
-        <ul class="pagination pagination-sm">
-          <li class="page-item disabled">
-            <a class="page-link" href="#">
-              <span aria-hidden="true">&laquo;</span>
-              <span>predhodna</span>
-            </a>
-          </li>
-          <li class="page-item active"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              <span>sledeća</span>
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
   
   </div>
