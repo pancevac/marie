a070c21be457fbc7be17505bd43a4b1e1866e4c4
VacaSan <kotaracvladan@gmail.com>
2018-07-19 14:54:56 +0200

    Add related sectin to gallery.

--- resources/views/site/pages/gallery.blade.php
+++ resources/views/site/pages/gallery.blade.php
@@ -34,6 +34,20 @@
           </simple-carousel>
 
         </div>
+
+        <div class="mb-5 pt-2">
+          <h2 class="h6 text-serif mb-3">Ne propustite</h2>
+
+          <div class="row">
+            @for($i = 0; $i < 6; $i++)
+            <div class="col col--6">
+              @related_item()
+              @endrelated_item
+            </div>
+            @endfor
+          </div>
+        </div>
+
       </div>
 
       <aside class="content-aside">
