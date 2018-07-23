dd415f9de8e5336922285c1e41c8b7bd121672fa
VacaSan <kotaracvladan@gmail.com>
2018-07-23 13:05:00 +0200

    Extracted social to partial.

--- resources/views/site/partials/sidenav.blade.php
+++ resources/views/site/partials/sidenav.blade.php
@@ -21,29 +21,7 @@
     </nav>
 
     <div>
-      <ul class="social">
-        <li class="social-item">
-          <a class="social-link" href="#" title="facebook">
-            <svg class="icon">
-              <use xlink:href="#icon_facebook"></use>
-            </svg>
-          </a>
-        </li>
-        <li class="social-item">
-          <a class="social-link" href="#" title="instagram">
-            <svg class="icon">
-              <use xlink:href="#icon_instagram"></use>
-            </svg>
-          </a>
-        </li>
-        <li class="social-item">
-          <a class="social-link" href="#" title="twitter">
-            <svg class="icon">
-              <use xlink:href="#icon_twitter"></use>
-            </svg>
-          </a>
-        </li>
-      </ul>
+      @include('site.partials.social')
     </div>
   </div>
 </drawer>
